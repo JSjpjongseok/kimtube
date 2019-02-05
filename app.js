2097a924d4cfabd6cc1c8b2a4201c926b4b77fab
Jongseok Lee <jongseoklee@JongseoknoiMac.local>
2019-02-05 15:27:40 +0900

    routers

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
+
 const app = express();
-
-const handleHome = (req, res) => res.send("Hello from sex!")
-
-const handleProfile = (req, res) =>  res.send("You are on my profile");
 
 // 유저로부터 받은 cookie를 이해하는 방법
 app.use(cookieParser());
@@ -17,13 +17,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(helmet());
 app.use(morgan("dev"));
-
-app.get("/",handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
-
 //누군가 내 파일을 불러올때 app object를 준다.
 //이게 제일 큰 덩어리
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
 export default app;
