4579f82addb4daa746fdb86013454c8e6e4719bc
Jongseok Lee <jongseoklee@JongseoknoiMac.local>
2019-02-06 10:51:09 +0900

    controller - pageTitle

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,14 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res ) => res.render("search");
+//pageTitle이 home템플릿으로 전달 되었다. 
+//home템플릿은 layouts/main 을 extend하고 있다.
+//home템플릿은 이 템플릿처럼 보인다는 뜻.
+//main.pug에 pageTitle변수가 표시될 자리가 있다.
 
-export const videos = (req, res ) => res.render("videos");
+export const home = (req, res) => res.render("home", { pageTitle: 'Home' });
+export const search = (req, res ) => res.render("search", { pageTitle: 'Search' });
 
-export const upload = (req, res) => res.render("upload")
-export const videoDetail = (req, res) => res.render("videoDetail")
-export const editVideo = (req, res) => res.render("editVideo")
-export const deleteVideo = (req, res) => res.render("deleteVideo")
+export const videos = (req, res ) => res.render("videos", { pageTitle: 'videos' })
+
+export const upload = (req, res) => res.render("upload", { pageTitle: 'upload' })
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: 'videoDetail' })
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle: 'editVideo' })
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: 'deleteVideo' })
